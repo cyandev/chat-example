1c03c8a04896615a7cc2fc6aff8bd4fffc3729a2
cyandev <djs.bitcoin@gmail.com>
2018-03-06 13:16:29 -0600

    Moving towards v1.0
    setting the stage for allowing longer messages

--- index.js
+++ index.js
@@ -2,14 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  var message = ""
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    if (msg.substr(0,3) != "NXT" && msg.substr(0,3) != "END") {
+      io.emit('chat message', msg);
+      message = ""3, str.length - 3
+      return;
+    }
+    message = message + msg.substr(3, str.length - 3)
+    if  (msg.substr(0,3) === "END") {
+       io.emit('chat message', message)
+       message = ""
+    }
+    
   });
 });
 
